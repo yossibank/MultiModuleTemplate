6f3a3e5f4cb7b5d23e1d21eed74ddabaaee92e3e
yossibank <yossibank@yahoo.co.jp>
2024-05-16 23:20:19 +0900

    Initial Commit

---
+++ MultiModuleTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiModuleTemplate
+//
+//  Created by Kamiyama Yoshihito on 2024/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiModuleTemplate/MultiModuleTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiModuleTemplateApp.swift
+//  MultiModuleTemplate
+//
+//  Created by Kamiyama Yoshihito on 2024/05/16.
+//
+
+import SwiftUI
+
+@main
+struct MultiModuleTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
